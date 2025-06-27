e769a24c1b45c027f8f8fdc9ab78e29aa6e4f0cb
awisbay <64637933+awisbay@users.noreply.github.com>
2025-06-27 22:45:00 +0700

    arrange susunan

--- main_page.py
+++ main_page.py
@@ -4,10 +4,10 @@ import streamlit as st
 g2l_generator = st.Page("g2l_st.py", title="GSM To LTE Script Generator", icon="⚙️")
 prepost_hc = st.Page("prepost_st.py", title="Pre/Post HC Log Generator", icon="🩺")
 polygon_app = st.Page("polygon_app.py", title="Polygon Converter", icon="✴️")
-modump_downloader = st.Page("getlistfile.py", title="Sunset Modump", icon="🔽")
+modump_downloader = st.Page("getlistfile.py", title="RNC Modump Sunset", icon="🔽")
 allip_downloader = st.Page("allip.py", title="Allip BSC", icon="🔽")
 migration = st.Page("migration.py", title="Migration Modump", icon="🔽")
-rbsdump = st.Page("rbsdump.py", title="RBS Sunset Modump", icon="🔽")
+rbsdump = st.Page("rbsdump.py", title="RBS Modump Sunset", icon="🔽")
 
 pg = st.navigation(
     {
