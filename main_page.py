3ee25dc371751bee84ad18f4c28153e7c7ab2067
awisbay <64637933+awisbay@users.noreply.github.com>
2025-06-27 22:14:34 +0700

    add allip

--- main_page.py
+++ main_page.py
@@ -5,12 +5,13 @@ g2l_generator = st.Page("g2l_st.py", title="GSM To LTE Script Generator", icon="
 prepost_hc = st.Page("prepost_st.py", title="Pre/Post HC Log Generator", icon="🩺")
 polygon_app = st.Page("polygon_app.py", title="Polygon Converter", icon="✴️")
 modump_downloader = st.Page("getlistfile.py", title="Modump Sunset", icon="🔽")
+allip_downloader = st.Page("allip.py", title="ALLIP BSC", icon="🔽")
 
 pg = st.navigation(
     {
         "2G Migration" : [g2l_generator, prepost_hc],
         "3G Migration" : [polygon_app],
-        "Downloader" : [modump_downloader]
+        "Downloader" : [modump_downloader, allip_downloader]
     }
 )
 st.set_page_config(page_title="IRS Migration Tools", page_icon="🛡", layout="wide")
